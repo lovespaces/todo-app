2aae911081cc6db6a3b220814fff2dabe4a930d3
love <74477945+lovespaces@users.noreply.github.com>
2026-02-28 12:18:52 +0900

    Update TaskProgButton.tsx

--- frontend/src/components/TaskProgButton.tsx
+++ frontend/src/components/TaskProgButton.tsx
@@ -1,20 +1,17 @@
 import { useQueryClient } from "@tanstack/react-query";
-import { client } from "../api/client";
+import { CompleteTask } from "../api/complete";
 
 function TaskProgressButton({ isToggle, taskId }: { isToggle: boolean, taskId: number }) {
     const queryClient = useQueryClient();
 
     const handleClick = async () => {
-        await client.db.completed[":id"].$patch({
-            param: { id: taskId.toString() },
-            json: { is_completed: !isToggle }
-        })
+        await CompleteTask(taskId, !isToggle);
 
         await queryClient.invalidateQueries({ queryKey: ['data'] })
     }
 
     return <>
-        <button onClick={handleClick} className={`${isToggle ? 'bg-white' : 'bg-transparent'} w-9 h-9 border rounded hover:cursor-pointer gap-2`}></button>
+        <button onClick={handleClick} className={`${isToggle ? 'bg-white' : 'bg-transparent'} w-9 h-9 border-2 rounded hover:cursor-pointer gap-2`}></button>
     </>
 }
 
