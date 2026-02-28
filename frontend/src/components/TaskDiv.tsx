bdba3408e4ab8d052ef7e77dc840771086dd25cf
love <74477945+lovespaces@users.noreply.github.com>
2026-02-28 12:18:45 +0900

    Update TaskDiv.tsx

--- frontend/src/components/TaskDiv.tsx
+++ frontend/src/components/TaskDiv.tsx
@@ -1,24 +1,23 @@
 import TaskProgressButton from "./TaskProgButton";
-import type { Dist } from "../types/db";
+import DeleteButton from "./DeleteButton";
+import type { GetDist } from "../types/db";
 
-type Props = {
-    data: Dist
-}
+function TaskDiv({ data, selectMode, setSelect }: { data: GetDist, selectMode: boolean, setSelect: (item: number, isDelete: boolean) => void }) {
 
-function TaskDiv({ data }: Props) {
+    const formatDate = (dateStr: string) => dateStr.replace(/-/g, '/');
 
     return (
         <>
             <div className="flex flex-col gap-6">
                 {data.map((task) => (
                     <div className="flex items-center gap-2" key={task.id}>
-                        <TaskProgressButton isToggle={task.is_completed} taskId={task.id} />
-                        <div className="flex-1 flex flex-col">
-                            <p className="font-bold">{task.name}</p>
+                        { selectMode ? <DeleteButton taskId={task.id} setSelect={setSelect} /> : <TaskProgressButton isToggle={task.is_completed} taskId={task.id} />}
+                        <div className={`flex-1 flex flex-col ${task.is_completed ? "opacity-50" : "opacity-100"}`}>
+                            <p className={`font-bold ${task.is_completed ? "line-through" : ""}`}>{task.name}</p>
                             <div className="flex justify-between opacity-75">
                                 <p>{task.description}</p>
                                 <p className="select-none">
-                                    {task.when_created}
+                                    {formatDate(task.when_created)}
                                 </p>
                             </div>
                         </div>
