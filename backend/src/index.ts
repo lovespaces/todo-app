09c19168adec309a053d2725dab405241c427ba4
love <74477945+lovespaces@users.noreply.github.com>
2026-02-28 12:20:32 +0900

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
-import { eq } from "drizzle-orm";
+import { eq, inArray } from "drizzle-orm";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { zValidator } from "@hono/zod-validator";
@@ -10,37 +10,57 @@ import { tasksTable } from "./db/schema";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
-function parseBool(v: string): boolean | null {
-    if (v === "true") return true;
-    if (v === "false") return false;
-
-    return null;
-}
-
 const app = new Hono()
     .use(cors({
         origin: process.env.FRONTEND_URL!,
     }))
     .get("/db", async (c) => {
         try {
-            const tasks = await db.select().from(tasksTable);
+            const tasks = await db.select().from(tasksTable).orderBy(tasksTable.id);
 
             return c.json(tasks, 200);
         } catch (e) {
             return c.json({ message: "DB Error" }, 500);
         }
     })
-    .patch("/db/completed/:id",
-        zValidator("json", z.object({ is_completed: z.boolean() })),
+    .post("/db/create",
+        zValidator("json", z.object({ name: z.string(), description: z.string()
+      })), async (c) => {
+        try {
+            const { name, description } = c.req.valid("json");
+
+            const task: typeof tasksTable.$inferInsert = {
+                name: name,
+                description: description,
+                is_completed: false
+            }
+
+            await db.insert(tasksTable).values(task);
+
+            return c.json({ message: "Added" }, 200);
+        } catch (e) {
+            return c.json({ message: "Error" }, 500);
+        }
+    })
+    .delete("/db/delete",
+        zValidator("json", z.object({ ids: z.array(z.number()) })),
         async (c) => {
-          const id: number = Number(c.req.param("id"));
+            try {
+                const { ids } = c.req.valid("json");
+                
+                await db.delete(tasksTable).where(inArray(tasksTable.id, ids))
 
-          if (!id) {
-              return c.json({ message: "UNKNOWN PARAM" }, 404);
-          }
-
+                return c.json({ message: "Deleted" }, 200);
+            } catch (e) {
+                return c.json({ message: "Error" }, 500);
+            }
+        }
+    )
+    .patch("/db/completed",
+        zValidator("json", z.object({ id: z.int(), is_completed: z.boolean() })),
+        async (c) => {
           try {
-              const { is_completed } = c.req.valid("json");
+              const { id, is_completed } = c.req.valid("json");
 
               await db
                   .update(tasksTable)
